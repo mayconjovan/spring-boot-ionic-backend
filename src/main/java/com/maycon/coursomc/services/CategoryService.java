b07088594d18e77b39cb7b61069949c3c60037c1
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-09 18:51:49 -0300

    Adjust in endpoint/categories/id and created exception class

--- src/main/java/com/maycon/coursomc/services/CategoryService.java
+++ src/main/java/com/maycon/coursomc/services/CategoryService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.maycon.coursomc.domain.Category;
 import com.maycon.coursomc.repositories.CategoryRepository;
+import com.maycon.coursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 
+	}
 }
