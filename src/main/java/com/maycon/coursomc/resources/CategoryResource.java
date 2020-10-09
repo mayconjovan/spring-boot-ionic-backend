bb11daf7c68b97c6543224b29fa6541e10bc7818
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-09 16:58:47 -0300

    Create repository and service to Category class.

--- src/main/java/com/maycon/coursomc/resources/CategoryResource.java
+++ src/main/java/com/maycon/coursomc/resources/CategoryResource.java
@@ -1,28 +1,26 @@
 package com.maycon.coursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maycon.coursomc.domain.Category;
+import com.maycon.coursomc.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Category> listar() {
-		Category cat = new Category(1, "Informática");
-		Category cat2 = new Category(2, "Escritório");
-		
-		List<Category> lista = new ArrayList<>();
-		lista.add(cat);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Category obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	
 	}
 }
