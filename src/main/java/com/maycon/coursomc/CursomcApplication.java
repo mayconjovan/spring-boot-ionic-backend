9f76a88eb3e54c0ea3a612d0a65975405da3fbf0
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-09 19:18:50 -0300

    Created State and City classes.

--- src/main/java/com/maycon/coursomc/CursomcApplication.java
+++ src/main/java/com/maycon/coursomc/CursomcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maycon.coursomc.domain.Category;
+import com.maycon.coursomc.domain.City;
 import com.maycon.coursomc.domain.Product;
+import com.maycon.coursomc.domain.State;
 import com.maycon.coursomc.repositories.CategoryRepository;
+import com.maycon.coursomc.repositories.CityRepository;
 import com.maycon.coursomc.repositories.ProductRepository;
+import com.maycon.coursomc.repositories.StateRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -19,31 +23,49 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
-	
+	@Autowired
+	private StateRepository stateRepository;
+	@Autowired
+	private CityRepository cityrepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
-		
+
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
-		
+
 		Product p1 = new Product(null, "Computador", 2000.00);
 		Product p2 = new Product(null, "Impressora", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
-		
+
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
-		
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		State est1 = new State(null, "Minas Gerais");
+		State est2 = new State(null, "São Paulo");
+
+		City c1 = new City(null, "Uberlândia", est1);
+		City c2 = new City(null, "São Paulo", est2);
+		City c3 = new City(null, "Campinas", est2);
+
+		est1.getCities().addAll(Arrays.asList(c1));
+		est2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		stateRepository.saveAll(Arrays.asList(est1, est2));
+		cityrepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
