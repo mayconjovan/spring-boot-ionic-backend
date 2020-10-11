cf11f45baafe95f27981e66f9835609218a11b86
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-11 15:39:57 -0300

    Inserting a new Category with POST Method

--- src/main/java/com/maycon/coursomc/resources/CategoryResource.java
+++ src/main/java/com/maycon/coursomc/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.maycon.coursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maycon.coursomc.domain.Category;
 import com.maycon.coursomc.services.CategoryService;
@@ -22,5 +26,13 @@ public class CategoryResource {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+	return ResponseEntity.created(uri).build();
 	}
 }
