81c9db9f5182483003a648ec146c0891efdbaa56
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-11 16:29:05 -0300

    Linting all categories with DTO

--- src/main/java/com/maycon/coursomc/resources/CategoryResource.java
+++ src/main/java/com/maycon/coursomc/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.maycon.coursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maycon.coursomc.domain.Category;
+import com.maycon.coursomc.dto.CategoryDTO;
 import com.maycon.coursomc.services.CategoryService;
 
 @RestController
@@ -45,5 +48,12 @@ public class CategoryResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/maycon/coursomc/services/CategoryService.java
+++ src/main/java/com/maycon/coursomc/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.maycon.coursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,9 @@ public class CategoryService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possue produtos");
 		}
+	}
+	
+	public List<Category> findAll(){
+		return repo.findAll();
 	}
 }
