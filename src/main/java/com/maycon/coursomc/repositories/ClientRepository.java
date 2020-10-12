cf35317cf497c2df1e1ddc592dd1aee66aa83b25
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-12 19:39:04 -0300

    Email validation customized

--- src/main/java/com/maycon/coursomc/repositories/ClientRepository.java
+++ src/main/java/com/maycon/coursomc/repositories/ClientRepository.java
@@ -2,10 +2,13 @@ package com.maycon.coursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.maycon.coursomc.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
+	@Transactional(readOnly=true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/maycon/coursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/maycon/coursomc/services/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.maycon.coursomc.domain.Client;
 import com.maycon.coursomc.domain.enums.TypeClient;
 import com.maycon.coursomc.dto.ClientNewDTO;
+import com.maycon.coursomc.repositories.ClientRepository;
 import com.maycon.coursomc.resources.exceptions.FieldMessage;
 import com.maycon.coursomc.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository repo;
+	
 	@Override
 	public void initialize(ClientInsert ann) {
 	}
@@ -28,12 +36,19 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		if (objDto.getType().equals(TypeClient.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "CNPJ inválido"));
 		}
+		
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
+		
+		
 		return list.isEmpty();
 	}
 }
