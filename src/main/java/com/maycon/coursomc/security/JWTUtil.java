cf7cabd5597277b0a113dc5852941bfab737f497
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-16 20:03:45 -0300

    Authorization implement

--- src/main/java/com/maycon/coursomc/security/JWTUtil.java
+++ src/main/java/com/maycon/coursomc/security/JWTUtil.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -19,16 +20,43 @@ public class JWTUtil {
 	private String expiration;
 
 	public String generateToken(String username) {
-		
+
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
 		cal.add(Calendar.MINUTE, 1);
-		cal.getTime();		
-		
-		return Jwts.builder()
-				.setSubject(username)
-				.setExpiration(cal.getTime())
-				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
-				.compact();
+		cal.getTime();
+
+		return Jwts.builder().setSubject(username).setExpiration(cal.getTime())
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
+	}
+
+	public boolean tokenValid(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+	
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
