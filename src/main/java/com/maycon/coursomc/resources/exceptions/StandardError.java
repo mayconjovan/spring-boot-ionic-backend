5d97f6338a60828572a714297661dcc879960a1f
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-17 19:23:22 -0300

    Patterning exception format

--- src/main/java/com/maycon/coursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/maycon/coursomc/resources/exceptions/StandardError.java
@@ -5,15 +5,27 @@ import java.io.Serializable;
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	private Long timestamp;
 	private Integer status;
+	private String error;
 	private String msg;
-	private Long timeStamp;
+	private String path;
 
-	public StandardError(Integer status, String msg, Long timeStamp) {
+	public StandardError(Long timestamp, Integer status, String error, String msg, String path) {
 		super();
+		this.timestamp = timestamp;
 		this.status = status;
+		this.error = error;
 		this.msg = msg;
-		this.timeStamp = timeStamp;
+		this.path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
 	}
 
 	public Integer getStatus() {
@@ -24,6 +36,14 @@ public class StandardError implements Serializable {
 		this.status = status;
 	}
 
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
 	public String getMsg() {
 		return msg;
 	}
@@ -32,14 +52,12 @@ public class StandardError implements Serializable {
 		this.msg = msg;
 	}
 
-	public Long getTimeStamp() {
-		return timeStamp;
+	public String getPath() {
+		return path;
 	}
 
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
+	public void setPath(String path) {
+		this.path = path;
 	}
-	
-	
 
 }
--- src/main/java/com/maycon/coursomc/resources/exceptions/ValidationError.java
+++ src/main/java/com/maycon/coursomc/resources/exceptions/ValidationError.java
@@ -8,8 +8,10 @@ public class ValidationError extends StandardError {
 
 	private List<FieldMessage> errors = new ArrayList<>();
 
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+
+
+	public ValidationError(Long timestamp, Integer status, String error, String msg, String path) {
+		super(timestamp, status, error, msg, path);
 	}
 
 	public List<FieldMessage> getErrors() {
