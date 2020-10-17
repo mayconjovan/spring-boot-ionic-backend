db29d6bbfed6c3f98ccc90cd1ec939d50a3611ed
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-17 16:51:11 -0300

    Adjusting image size

--- src/main/java/com/maycon/coursomc/services/ClientService.java
+++ src/main/java/com/maycon/coursomc/services/ClientService.java
@@ -50,6 +50,9 @@ public class ClientService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Client find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -128,6 +131,11 @@ public class ClientService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
