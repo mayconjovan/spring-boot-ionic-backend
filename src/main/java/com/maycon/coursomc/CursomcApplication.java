69c20f286df1d164e0825f7c0eb14da40f508e22
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-17 15:17:32 -0300

    Sending image with endpoint

--- src/main/java/com/maycon/coursomc/CursomcApplication.java
+++ src/main/java/com/maycon/coursomc/CursomcApplication.java
@@ -1,27 +1,18 @@
 package com.maycon.coursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.maycon.coursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3service;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
-
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3service.uploadFile("C:\\temp\\fotos\\goku.jpg");
-
 	}
 
 }
--- src/main/java/com/maycon/coursomc/services/ClientService.java
+++ src/main/java/com/maycon/coursomc/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.maycon.coursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.maycon.coursomc.domain.Adress;
 import com.maycon.coursomc.domain.City;
@@ -37,6 +39,9 @@ public class ClientService {
 
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Client find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -107,5 +112,9 @@ public class ClientService {
 		newObj.setEmail(obj.getEmail());
 	}
 	
+	
+	public URI uploadProfilePicture (MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
 	
 }
